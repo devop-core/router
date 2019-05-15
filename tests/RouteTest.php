af9c873b77506c4c224cedb9049fe9da9f5613b1
Zlatin Hristov <zlatin.hristov@devop.studio>
2019-05-15 15:34:53 +0300

    Remove PHP5 support

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -1,25 +1,27 @@
 <?php
 namespace DevOp\Core\Router\Test;
 
-class RouteTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class RouteTest extends TestCase
 {
 
     /**
      * @var \DevOp\Core\Router\Route
      */
     private $route;
-    
+
     public function setUp()
     {
         $this->route = new \DevOp\Core\Router\Route('/', 'handler01');
     }
-    
+
     public function testGetMethods()
     {
         $this->assertEquals('/', $this->route->getPattern());
         $this->assertEquals('handler01', $this->route->getCallback());
     }
-    
+
     public function testAddRoute()
     {
         $route = new \DevOp\Core\Router\Route('/', 'SomeController@SomeMethod');
--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace DevOp\Core\Router\Test;
 
-class RouterTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
 {
 
     /**
@@ -19,43 +21,43 @@ class RouterTest extends \PHPUnit_Framework_TestCase
         $router = $this->router->add('homepage', ['GET'], '/', 'handler01');
         $this->assertInstanceOf('\DevOp\Core\Router\Router', $router);
     }
-    
+
     public function testGetMethod()
     {
         $router = $this->router->get('route01', '/', 'handler01');
         $this->assertInstanceOf('\DevOp\Core\Router\Router', $router);
     }
-    
+
     public function testPostMethod()
     {
         $router = $this->router->get('route02', '/', 'handler02');
         $this->assertInstanceOf('\DevOp\Core\Router\Router', $router);
     }
-    
+
     public function testOptionsMethod()
     {
         $router = $this->router->get('route03', '/', 'handler03');
         $this->assertInstanceOf('\DevOp\Core\Router\Router', $router);
     }
-    
+
     public function testHeadMethod()
     {
         $router = $this->router->get('route04', '/', 'handler04');
         $this->assertInstanceOf('\DevOp\Core\Router\Router', $router);
     }
-    
+
     public function testRouterThrowIsNotCallableException()
     {
-        $this->setExpectedException('\DevOp\Core\Router\Exceptions\RouteIsNotCallableException');
+        $this->expectException('\DevOp\Core\Router\Exceptions\RouteIsNotCallableException');
         $uri = (new \DevOp\Core\Http\UriFactory())->createUri('/');
         $request = (new \DevOp\Core\Http\RequestFactory())->createRequest('GET', $uri);
         $router = $this->router->add('homepage', ['GET'], '/', 'handler01');
         $router->dispatch($request);
     }
-    
+
     public function testRouterThroNotFoundException()
     {
-        $this->setExpectedException('\DevOp\Core\Router\Exceptions\RouteNotFoundException');
+        $this->expectException('\DevOp\Core\Router\Exceptions\RouteNotFoundException');
         $uri = (new \DevOp\Core\Http\UriFactory())->createUri('/');
         $request = (new \DevOp\Core\Http\RequestFactory())->createRequest('POST', $uri);
         $router = $this->router->add('homepage', ['GET'], '/', 'handler01');
